7da71ca65f6ed4fd84d60ac4de18a14032e5bae3
Team6-2025 <s-boselli.lorenzo@liceorespighi.it>
2025-01-17 19:21:59 +0100

    INtro

---
+++ grafica.js
@@ -0,0 +1,39 @@
+function clearScreen(){
+    while (field.firstChild) {
+        field.removeChild(field.firstChild);
+    }
+}
+
+
+const quoteContainer = document.createElement("p");
+const quote = document.createElement("p");
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    quoteContainer.classList.add("Platone");
+    const text = '"La meraviglia è il principio della filosofia, perchè è dal sentirsi stupiti che nasce il desiderio di conoscere"';
+    const speed = 50; 
+    let i = 0;
+    quote.classList.add("Aristotele");
+    quote.innerHTML = "[Aristotele]";
+    
+
+    field.appendChild(quoteContainer);
+    field.appendChild(quote);
+    function typeWriter() {
+        if (i < text.length) {
+            quoteContainer.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+    typeWriter();
+
+    setTimeout(()=>quote.classList.add("Visible"), speed*text.length * 3/2);
+    });
+
+quoteContainer.addEventListener("click", ()=>{
+    quoteContainer.style.transform = "scale(50) translateZ(1000px)";
+    setTimeout(()=>{clearScreen()}, 800);
+
+});
